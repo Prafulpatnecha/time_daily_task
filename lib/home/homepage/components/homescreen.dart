66d5faaa0d2a4b473a97da18899eaabf6bd9e317
Praful Patnecha <prafulpatnecha@gmail.com>
2024-04-30 10:34:09 +0530

    30-04-24

--- lib/home/homepage/components/homescreen.dart
+++ lib/home/homepage/components/homescreen.dart
@@ -16,7 +16,6 @@ class Button extends StatelessWidget {
     );
   }
 }
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -25,13 +24,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+Calendar calendar= Calendar.week;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: Container(
         color: (colors == false) ? Colors.white : Colors.cyanAccent,
         height: 80,
-        child: (profile==false)?buildRow():const Center(child: Text('Butten Is All Work For Different',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),)),
+        child: (profile == false) ? buildRow() : const Center(child: Text(
+          'Butten Is All Work For Different', style: TextStyle(
+            color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),)),
       ),
       body: Container(
         color: (colors == true) ? Colors.white : Colors.cyanAccent,
@@ -66,30 +68,43 @@ class _HomePageState extends State<HomePage> {
                 icon: Icon(
                   Icons.account_circle_outlined,
                   size: 50,
-                  color: (colo==false)?Colors.white:Colors.black,
+                  color: (colo == false) ? Colors.white : Colors.black,
                 ),
               ),
               OutlinedButton(onPressed: () {
                 setState(() {
-                  if(colo==true){
-                    colo=false;
-                  }else{
-                    colo=true;
+                  if (colo == true) {
+                    colo = false;
+                  } else {
+                    colo = true;
                   }
                 });
               },
-                child: const Text('Butten Color',style: TextStyle(fontSize: 30),),),
+                child: const Text(
+                  'Butten Color', style: TextStyle(fontSize: 30),),),
               ElevatedButton(onPressed: () {
                 setState(() {
-                  if(first==true){
-                    first=false;
+                  if (first == true) {
+                    first = false;
                   }
-                  else{
-                    first=true;
+                  else {
+                    first = true;
                   }
                 });
               }, child: const Text('Last Button'),),
-              (first==false)?buildContainer():Container(),
+              (first == false) ? buildContainer() : Container(),
+              SegmentedButton(
+                  segments: [
+                    ButtonSegment(value: Calendar.day,label: Text('day'),icon: Icon(Icons.calendar_view_day)),
+                    ButtonSegment(value: Calendar.week,label: Text('Week'),icon: Icon(Icons.view_week)),
+                  ], selected: <Calendar>{
+                    calendar,
+              },
+                onSelectionChanged: (p0) {
+                    setState(() {
+                      calendar= p0.first ;
+                    });
+                },)
             ],
           ),
         ),
@@ -99,29 +114,31 @@ class _HomePageState extends State<HomePage> {
 
   Container buildContainer() {
     return Container(
-          height: 250,
-          width: 250,
-          decoration: BoxDecoration(
-            image: DecorationImage(
+      height: 250,
+      width: 250,
+      decoration: BoxDecoration(
+          image: DecorationImage(
               image: ram
-            )
-          ),
-        );
+          )
+      ),
+    );
   }
 
   Row buildRow() {
     return Row(
-        children: [
-          const SizedBox(width: 20,),
-          Container(
-            height: 50,width: 50,
-            decoration: BoxDecoration(
-              image: DecorationImage(image: pro,),
-            ),
+      children: [
+        const SizedBox(width: 20,),
+        Container(
+          height: 50, width: 50,
+          decoration: BoxDecoration(
+            image: DecorationImage(image: pro,),
           ),
-          const SizedBox(width: 20,),
-          const Text('Akhil Sodvadiya',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 25),),
-        ],
-      );
+        ),
+        const SizedBox(width: 20,),
+        const Text('Akhil Sodvadiya', style: TextStyle(
+            color: Colors.black, fontWeight: FontWeight.bold, fontSize: 25),),
+      ],
+    );
   }
 }
+enum Calendar{day,week}
